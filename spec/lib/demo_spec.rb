424f0b874495dd89757ea1a27b120e78ef0af092
Brooke Kuhlmann <brooke@alchemists.io>
2026-05-23 17:03:41 -0600

    Added demonstration
    Provides a quick way to see what this implementation can do. This demonstrates a slide show consisting of three slides which you can navigate to next, previous, last, and first. This includes toggling fullscreen.

--- spec/lib/demo_spec.rb
+++ spec/lib/demo_spec.rb
@@ -26,4 +26,77 @@ RSpec.describe "Demo", :js do
     visit "/index.html"
     expect(page).to have_text("Demonstration")
   end
+
+  it "toggles fullscreen", :aggregate_failures do
+    fullscreen = proc do
+      page.evaluate_script "document.fullscreenElement === arguments[0]", find(".viewport")
+    end
+
+    visit "/index.html"
+    click_button "[ ]"
+
+    expect(fullscreen.call).to be(true)
+
+    find("body").send_keys "f"
+
+    expect(fullscreen.call).to be(false)
+
+    find("body").send_keys "f"
+
+    expect(fullscreen.call).to be(true)
+
+    find("body").send_keys "f"
+    click_link "Next"
+    find("body").send_keys "f"
+
+    expect(fullscreen.call).to be(true)
+  end
+
+  it "plays slideshow", :aggregate_failures do
+    visit "/index.html"
+
+    expect(page).to have_css(%(#progress[value="0"]))
+    expect(page).to have_css(%(#progress[max="2"]))
+    expect(page).to have_text("1 of 3")
+
+    click_link "Next"
+
+    expect(page).to have_css(%(#progress[value="1"]))
+    expect(page).to have_text("2 of 3")
+
+    find("body").send_keys :right
+
+    expect(page).to have_css(%(#progress[value="2"]))
+    expect(page).to have_text("3 of 3")
+
+    click_link "Previous"
+
+    expect(page).to have_css(%(#progress[value="1"]))
+    expect(page).to have_text("2 of 3")
+
+    find("body").send_keys :left
+
+    expect(page).to have_css(%(#progress[value="0"]))
+    expect(page).to have_text("1 of 3")
+
+    click_link "First"
+
+    expect(page).to have_css(%(#progress[value="0"]))
+    expect(page).to have_text("1 of 3")
+
+    click_link "Last"
+
+    expect(page).to have_css(%(#progress[value="2"]))
+    expect(page).to have_text("3 of 3")
+
+    find("body").send_keys "["
+
+    expect(page).to have_css(%(#progress[value="0"]))
+    expect(page).to have_text("1 of 3")
+
+    find("body").send_keys "]"
+
+    expect(page).to have_css(%(#progress[value="2"]))
+    expect(page).to have_text("3 of 3")
+  end
 end
